be46dba6617d99f0ed27752718e62e58fe9e5fb8
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-06 21:37:48 -0400

    Added Player/Game Models

--- lib/hangr.rb
+++ lib/hangr.rb
@@ -2,11 +2,22 @@ require "hangr/version"
 require "hangr/init_db"
 require "pry"
 
-module Hangr
-  # Your code goes here...
+module Hangr # Module helps group methods, classes, constants. They avoid name conflicts among classes and provivide name space. Mixin facility?
+ 
+  
 
-  class Player < ActiveRecord::Base
+  class Player < ActiveRecord::Base # Creates active record model; a Player model
+    name :name # attributes of the instances of the model.
+    age :age # attributes of the instances of the model.
+    total_wins :total_wins  # attributes of the instances of the model.
+  end
+
+  class Game < ActiveRecord::Base # Creates active record model; a Game model
+    word :current_word
+    turns :turns
   end
 end
 
-binding.pry
+p = Player.new
+p.name
+puts p.name
